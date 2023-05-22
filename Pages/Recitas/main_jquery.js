344587ba5363abcd22c2285a65244bfa7dc20515
Wesley Silva C <wesleysilvaconceicao@outlook.com>
2023-05-22 15:01:00 -0300

    Finaliazação do cards e buttons da página de receitas.

--- Pages/Recitas/main_jquery.js
+++ Pages/Recitas/main_jquery.js
@@ -1,7 +1,24 @@
+// Seleção dos botões e conteúdos dos parâmetros
 $(function () {
-  $('.btn').on({
-    click: function () {
-      $('.receitas-conteudo').toggle(500)
-    }
+  $('.btn-400').click(function () {
+    $('.content-400').toggle(500)
+  })
+  $('.btn-600').click(function () {
+    $('.content-600').toggle(500)
+  })
+  $('.btn-720').click(function () {
+    $('.content-720').toggle(500)
+  })
+  $('.btn-105').click(function () {
+    $('.content-105').toggle(500)
+  })
+  $('.btn-15').click(function () {
+    $('.content-15').toggle(500)
+  })
+  $('.btn-20').click(function () {
+    $('.content-20').toggle(500)
+  })
+  $('.btn-25').click(function () {
+    $('.content-25').toggle(500)
   })
 })

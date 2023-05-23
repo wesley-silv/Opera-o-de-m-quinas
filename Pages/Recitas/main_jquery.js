546c3165393d44e5590b32471095be50e05417bd
Wesley Silva C <wesleysilvaconceicao@outlook.com>
2023-05-22 22:37:31 -0300

    Início do projeot operação de máquinas.

--- Pages/Recitas/main_jquery.js
+++ Pages/Recitas/main_jquery.js
@@ -1,24 +1,24 @@
 // Seleção dos botões e conteúdos dos parâmetros
 $(function () {
   $('.btn-400').click(function () {
-    $('.content-400').toggle(500)
+    $('.content-400').slideToggle(500)
   })
   $('.btn-600').click(function () {
-    $('.content-600').toggle(500)
+    $('.content-600').slideToggle(500)
   })
   $('.btn-720').click(function () {
-    $('.content-720').toggle(500)
+    $('.content-720').slideToggle(500)
   })
   $('.btn-105').click(function () {
-    $('.content-105').toggle(500)
+    $('.content-105').slideToggle(500)
   })
   $('.btn-15').click(function () {
-    $('.content-15').toggle(500)
+    $('.content-15').slideToggle(500)
   })
   $('.btn-20').click(function () {
-    $('.content-20').toggle(500)
+    $('.content-20').slideToggle(500)
   })
   $('.btn-25').click(function () {
-    $('.content-25').toggle(500)
+    $('.content-25').slideToggle(500)
   })
 })
